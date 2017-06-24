068519444754528346d58c787540d02bcc51948c
soomingjin <mingjin_soo@mymail.sutd.edu.sg>
2017-06-24 21:36:28 +0800

    Added functionality to the page, task is completed , but requires abit more functionality and design # ------------------------ >8 ------------------------ # Do not touch the line above. # Everything below will be removed. # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch master # Your branch is up-to-date with 'origin/master'. # Changes to be committed: # # 	modified:   index.html # 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,30 +1,57 @@
+
+var images = [ 'http://thecatapi.com/api/images/get.php?api_key=MTAx&id=cfc',
+  'https://images.pexels.com/photos/127028/pexels-photo-127028.jpeg?h=350&auto=compress&cs=tinysrgb',
+  'https://images.pexels.com/photos/57416/cat-sweet-kitty-animals-57416.jpeg?h=350&auto=compress&cs=tinysrgb' ];
+
+
+function leftButton(currentIndex) {
+  $('#output').text("Left Button Clicked");
+  var nextIndex = currentIndex;
+  if (currentIndex <= 0) {
+    nextIndex = images.length - 1;
+  } else {
+    nextIndex -= 1;
+  }
+  $('#indexOutput').text(nextIndex);
+  showImage(nextIndex);
+  return;
+}
+
+function rightButton(currentIndex) {
+  $('#output').text("Right Button Clicked");
+  var nextIndex = currentIndex;
+  if (currentIndex >= (images.length - 1)) {
+    nextIndex = 0;
+  } else {
+    nextIndex += 1;
+  }
+  $('#indexOutput').text(nextIndex);
+  showImage(nextIndex);
+  return;
+}
+
+function showImage(index) {
+  var nextSource = images[index];
+  var img = $('#image-container');
+  img.attr('src', nextSource);
+  return;
+}
+
+function getImageIndex(){
+  var img = $('#image-container');
+  var source = img.attr('src');
+  return images.indexOf(source);
+}
+
+function init(){
+  var currIndex;
+  currIndex = 0;
+  $('#indexOutput').text(currIndex);
+  showImage(currIndex);
+}
+
 $(function() {
 
-  images = [];
+  init();
 
-  function leftButton(currentIndex) {
-    var nextIndex;
-    if (currentIndex <= 0) {
-      nextIndex = images.length - 1;
-    } else {
-      nextIndex -= 1;
-    }
-    changeImage(nextIndex);
-    return;
-  }
-
-  function rightButton(currentIndex) {
-    if (currentIndex >= (images.length - 1)) {
-      nextIndex = 0;
-    } else {
-      nextIndex += 1;
-    }
-    changeImage(nextIndex);
-    return;
-  }
-
-  function changeImage(index) {
-    var nextSource = images[index];
-
-  }
 });
