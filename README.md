649ba57d235c76f6bb7d0c820a0658f3ead243fb
soomingjin <mingjin_soo@mymail.sutd.edu.sg>
2017-07-06 15:00:24 +0800

    Completed project, refactored code, learned new things. Now moving on converting to ES6 syntax

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ This side project is a way for me to test some of my web dev skills.
 
 The website should have a container to display a cat image and there should be buttons by its left and right that can cycle the images in it.
 
+# Demo
+[Link here](https://soomingjin.github.io/photocarousell/)
+
 # Dependencies
 * jQuery
 * Bootstrap
@@ -11,6 +14,9 @@ The website should have a container to display a cat image and there should be b
 # Learning Points
 * Code Documentation
 It is essential as it helps me to make the code more readable
+Use `data-*` attributes to store global variables in the DOM to prevent memory leaks
+Prefix `'$'`  to jQuery variables
+
 
 * CSS3
 I learned how to use different properties and understood a few concepts about formatting
--- js/script.js
+++ js/script.js
@@ -1,117 +1,100 @@
-//Array that stores the sources of images
-var images = ['http://thecatapi.com/api/images/get.php?api_key=MTAx&id=cfc',
-  'https://images.pexels.com/photos/127028/pexels-photo-127028.jpeg?h=350&auto=compress&cs=tinysrgb',
-  'https://images.pexels.com/photos/57416/cat-sweet-kitty-animals-57416.jpeg?h=350&auto=compress&cs=tinysrgb',
-  'https://static.pexels.com/photos/236603/pexels-photo-236603.jpeg',
-  'https://static.pexels.com/photos/209800/pexels-photo-209800.jpeg',
-  'https://static.pexels.com/photos/199449/pexels-photo-199449.jpeg',
-  'https://static.pexels.com/photos/87413/animal-cat-domestic-eye-87413.jpeg',
-  'https://static.pexels.com/photos/64147/cat-young-cat-playful-pet-64147.jpeg'
-];
+$(function() {
+  // image array to store all the properties of the cat images, namely the path
+  // and the caption.
+  var images = [{
+    path: "images/cate2.jpeg",
+    caption: "Curious grey kitten"
+  },{
+    path: "images/cate3.jpeg",
+    caption: "Cat licking off the last bits of his meal from its paw"
+  },{
+    path: "images/cate4.jpg",
+    caption: "The cat stays still, keeping in mind not to startle its prey"
+  },{
+    path: "images/cate6.jpg",
+    caption: "After destroying its toy, it looks for a new target"
+  },{
+    path: "images/cate7.jpg",
+    caption: "The white furred cat stares into the camera lens, admiring its beautiful coloured green eyes"
+  }
+  ];
 
-var localImages = [
-  // 'images/cate1.jpg',
-  'images/cate2.jpeg',
-  'images/cate3.jpeg',
-  'images/cate4.jpg',
-  // 'images/cate5.jpg',
-  'images/cate6.jpg',
-  'images/cate7.jpg',
-  // 'images/cate8.jpg'
-];
+  /**
+   *Controls how the carousell buttons behave when clicked
+   *@param {Number} index of image
+   */
+  function carousellButtonHandler(event) {
+    //obtain the current index
+    //find the name of the button being pressed
+    var nextIndex = getImageIndex();
+    var buttonType = event.target.name;
+    var $img = $('#cat-image');
 
-var imageCaptions = [
-  // "placeholder for caption",
-  "Curious grey kitten",
-  "Cat licking off the last bits of his meal from its paw",
-  "The cat stays still, keeping in mind not to startle its prey",
-  // "placeholder for caption",
-  "After destroying its toy, it looks for a new target",
-  "The white furred cat stares into the camera lens, admiring its beautiful coloured green eyes",
-  // "placeholder for caption",
-
-];
-
-/**
- *Controls how the carousell buttons behave when clicked
- *@param {Number} index of image
- */
-function carousellButtonHandler(event) {
-  //obtain the current index
-  //find the name of the button being pressed
-  var nextIndex = getImageIndex();
-  var buttonType = this.name;
-
-  //check which button is being pressed
-  if (buttonType === "next") {
-    if (nextIndex >= (localImages.length - 1)) {
-      nextIndex = 0;
+    //check which button is being pressed
+    if (buttonType === "next") {
+      if (nextIndex >= images.length - 1) {
+        nextIndex = 0;
+      } else {
+        nextIndex += 1;
+      }
     } else {
-      nextIndex += 1;
+      if (nextIndex <= 0) {
+        nextIndex = images.length - 1;
+      } else {
+        nextIndex -= 1;
+      }
     }
-  } else {
-    if (nextIndex <= 0) {
-      nextIndex = localImages.length - 1;
-    } else {
-      nextIndex -= 1;
-    }
+    $img.data('current-index', nextIndex);
+    showImage(nextIndex);
+    return;
   }
 
-  showImage(nextIndex);
-  return;
-}
+  /**
+   *Returns the index of the image in image array
+   *
+   *@return {Number} index of image
+   */
+  function getImageIndex() {
+    var $img = $("#cat-image");
+    return $img.data('current-index');
+  }
 
-/**
- *Returns the index of the image in image array
- *
- *@return {Number} index of image
- */
-function getImageIndex() {
-  var img = $('#cat-image');
-  var source = img.attr('src');
-  return localImages.indexOf(source);
-}
+  /**
+   *Modifies the source attribute of the container to display an image
+   *@param {Number} index of image
+   */
+  function showImage(index) {
+    var nextSource = images[index].path;
+    var $img = $("#cat-image");
+    var $caption = $(".carousel-caption");
+    $img
+      .hide({
+        effect: "fade",
+        duration: 100,
+        complete: function() {
+          $img.attr("src", nextSource);
+          $caption.text(images[index].caption);
+        }
+      })
+      .show({
+        effect: "fade",
+        duration: 100
+      });
+    return;
+  }
 
-/**
- *Modifies the source attribute of the container to display an image
- *@param {Number} index of image
- */
-function showImage(index) {
-  // var nextSource = images[index];
-
-  var nextSource = localImages[index];
-  var img = $('#cat-image');
-  var caption = $('.carousel-caption');
-  img.hide({
-    effect: 'fade',
-    duration: 100,
-    complete: function() {
-      img.attr('src', nextSource);
-      caption.text(imageCaptions[index]);
-    }
-  }).show({
-    effect: 'fade',
-    duration: 100,
-  });
-  $('#indexOutput').text(index);
-  return;
-}
-
-
-
-//Run this function when the website is loaded for the first time
-//To show the first image in the array
-function init() {
-  var currIndex;
-  currIndex = 0;
-  showImage(currIndex);
-  //tag event handlers to buttons
-  $('.carousellButton').click(carousellButtonHandler);
-  $('.carousellButton').height($('#cat-image').height());
-}
-
-$(function() {
-
+  //Run this function when the website is loaded for the first time
+  //To show the first image in the array
+  function init() {
+    var currIndex = 0;
+    var $img = $("#cat-image");
+    $img.data('current-index', currIndex);
+    //show first image
+    showImage($img.data('current-index'));
+    //tag event handlers to buttons
+    $(".carousellButton").click(carousellButtonHandler);
+    $(".carousellButton").height($img.height());
+  }
+  
   init();
-
 });
