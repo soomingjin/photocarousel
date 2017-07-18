611a977b847645ff1eb1f71bbd63f89a5c2f0efd
soomingjin <mingjin_soo@mymail.sutd.edu.sg>
2017-07-18 16:14:16 +0800

    Added search functionality to the webpage

--- js/script.js
+++ js/script.js
@@ -43,7 +43,16 @@ $(() => {
     const $catdivs = $('.cat-div');
     const $active = $('.active');
     const $nextSlide = $catdivs.eq(nextIndex);
+    const $imageContainer = $('.image-container');
+    const containerHeight = $imageContainer.height();
+    const containerWidth = $imageContainer.width();
     if (buttonType === 'init') {
+      // $.each($catdivs, function(i, v) {
+      //   let $that = $(this);
+      //   const ratio = calculateAspectRatioFit($that.width(), $that.height(), containerWidth, containerHeight);
+      //   $that.width(ratio.width);
+      //   $that.height(ratio.height);
+      // });
       $('.cat-div').eq(nextIndex).toggleClass('active');
     } else {
       animateSlide($active, $nextSlide, buttonType);
@@ -51,6 +60,24 @@ $(() => {
 
     return;
   }
+
+  /**
+ * Conserve aspect ratio of the orignal region. Useful when shrinking/enlarging
+ * images to fit into a certain area.
+ *
+ * @param {Number} srcWidth Source area width
+ * @param {Number} srcHeight Source area height
+ * @param {Number} maxWidth Fittable area maximum available width
+ * @param {Number} maxHeight Fittable area maximum available height
+ * @return {Object} { width, heigth }
+ */
+  function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
+
+     var ratio = Math.floor(Math.min(maxWidth / srcWidth, maxHeight / srcHeight));
+
+     return { width: srcWidth*ratio, height: srcHeight*ratio };
+  }
+
 
   /*
    * Animates the carousel sliding
@@ -114,34 +141,68 @@ $(() => {
   // feature not ready yet
   function fetchImageHandler(event) {
     event.preventDefault();
-    const keywords = $("#fetch-keywords").val().trim().split(/\s+/);
+    const keywords = $("#fetch-keywords").val().trim().split(/\s+/).join(',');
     const apiKey = "fd5f20a53c009a33506904c2ab164800";
-    const flickrurl = "https://api.flickr.com/services/rest/?";
+    const flickrurl = "https://api.flickr.com/services/rest/";
     console.log('execute');
-    $.ajax({
-      dataType: "xml",
+    var xhr = $.ajax({
+      dataType: "json",
       url: flickrurl,
       data: {
         method: "flickr.photos.search",
         api_key: apiKey,
-        tags: 'superrare',
+        format: 'json',
+        nojsoncallback: 1,
+        tags: keywords,
         safe_search: 1,
-        content_type: 4
+        content_type: 4,
+        per_page: 20
       }
-    }).done(function(data) {
-      console.log(data);
-      console.log($.parseXML(data));
-      // $.each(data.photos, function(i,item){
-      //   var photoURL = 'http://farm' + item.farm + '.static.flickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_m.jpg';
-      //   console.log();
-      //   //turn the photo id into a variable
-      //   var photoID = item.id;
-      // });
+    }).done(function(rsp, textStatus, jqXHR) {
+      console.log('done');
+      if (rsp.stat !== 'ok'){
+        console.log(rsp.stat + rsp.code + rsp.message);
+        console.log('not ok');
+        return;
+      } else {
+        const $removedDivs = $('.cat-div').detach();
+        const $imageContainer = $('.image-container');
+        for (let i = 0; i < rsp.photos.photo.length; i++) {
+          let photo = rsp.photos.photo[i];
+          let id= photo.id;
+          let farmId = photo.farm;
+          let serverId = photo.server;
+          let secret = photo.secret;
+          let photoTitle = photo.title;
+          let mstzb = 'z'; //photosize
+          let photoURL = `https://farm${farmId}.staticflickr.com/${serverId}/${id}_${secret}_${mstzb}.jpg`
+          // (secondary) loading gif to be added over the container
+          // make elements to be added into the dom
+          // remove current images (possibly store them)
+          // reset the index -> display the images
+
+          // div -> images
+          let img = document.createElement('img');
+          let div = document.createElement('div');
+          img.setAttribute('class', 'cat-image');
+          img.setAttribute('src', photoURL);
+          img.setAttribute('title', photoTitle);
+          // div.append(img);
+          div.setAttribute('class', 'cat-div');
+          div.setAttribute('background-image', `url(\"${photoURL}\") no-repeat center center`);
+          $imageContainer.append(div);
+
+        }
+        $imageContainer.data('current-index', 0);
+        render(0);
+      }
+      return;
 
     }).fail(function(jqXHR, textStatus, errorThrown) {
       console.log('fail');
-    })
-
+      console.log('textStatus = ' + textStatus);
+      console.log(errorThrown);
+    });
 
     return;
   }
