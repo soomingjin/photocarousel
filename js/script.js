9072d8ee6a681050806534237cd9e94c860c9d56
soomingjin <mingjin_soo@mymail.sutd.edu.sg>
2017-07-26 17:25:10 +0800

    Edited style, Added new button to reset images and made it easier to initialise photocarousel

--- js/script.js
+++ js/script.js
@@ -19,6 +19,7 @@ $(() => {
     caption: "The white furred cat stares into the camera lens, admiring its beautiful coloured green eyes"
   }];
 
+
   function buildLocalPath(name) {
     return `images/${name}`;
   }
@@ -29,13 +30,13 @@ $(() => {
     const $photocarousel = event.data.photocarousel;
     const $photocarouselDivs = $photocarousel.children('.photocarousel-div');
     const buttonType = this.name;
-    const buttonActive = $photocarousel.data('active');
-    const currentIndex = $photocarousel.data('current-index');
+    const buttonActive = $photocarousel.photocarouselData.active;
+    const currentIndex = $photocarousel.photocarouselData.currentIndex;
     let nextIndex = currentIndex;
 
     //check which button is being pressed and determine the next index
     if (buttonActive) {
-      $photocarousel.data('active', false);
+      $photocarousel.photocarouselData.active = false;
       if (buttonType === "next" && $photocarouselDivs.length > 1) {
         if (nextIndex >= $photocarouselDivs.length - 1) {
           nextIndex = 0;
@@ -58,15 +59,16 @@ $(() => {
       return;
     }
 
-    $photocarousel.data('current-index', nextIndex);
-    render(nextIndex, $photocarousel, buttonType);
+    // $photocarousel.data('current-index', nextIndex);
+    $photocarousel.photocarouselData.currentIndex = nextIndex;
+    render({}, nextIndex, $photocarousel, buttonType);
     return;
   }
 
   /*
    * Handles the logic of the next slide to be displayed
    */
-  function render(nextIndex, $photocarousel, buttonType = 'init', opts) {
+  function render(opts, nextIndex, $photocarousel, buttonType = 'init') {
     let $photocarouselDivs = $photocarousel.children('.photocarousel-div');
     let $active = $photocarousel.children('.activeSlide');
     let $nextSlide = $photocarouselDivs.eq(nextIndex);
@@ -98,7 +100,9 @@ $(() => {
    * @returns {boolean} success/fail
    */
   function buildDOM($photocarousel, buttonType, opts) {
+    const $removedDivs = $photocarousel.children('.photocarousel-div').detach();
     if (buttonType === 'init') {
+      createButtons(opts.numberOfSlides, $photocarousel);
       createLoaderDiv($photocarousel);
       for (let i = 0; i < opts.numberOfSlides; i++) {
         let imagePath = buildLocalPath(images[i].path);
@@ -106,7 +110,6 @@ $(() => {
         createPhotocarouselDiv(imagePath, imageCaption, $photocarousel);
       }
       createSearchFunction(opts.searchFunction, $photocarousel);
-      return true;
     } else if (buttonType === 'fetch') {
       const data = opts.data;
       for (let i = 0; i < data.length; i++) {
@@ -117,7 +120,7 @@ $(() => {
         let secret = photo.secret;
         let photoTitle = photo.title;
         let mstzb = 'z'; //photosize
-        let photoURL = `https://farm${farmId}.staticflickr.com/${serverId}/${id}_${secret}_${mstzb}.jpg`
+        let photoURL = `https://farm${farmId}.staticflickr.com/${serverId}/${id}_${secret}_${mstzb}.jpg`;
         // (secondary) loading gif added over the container
         // make elements to be added into the dom
         // remove current images (possibly store them)
@@ -127,9 +130,21 @@ $(() => {
 
         createPhotocarouselDiv(photoURL, photoTitle, $photocarousel);
       }
-      return true;
+
     }
-    return false;
+    return ;
+  }
+
+  function createButtons(numberOfSlides, $photocarousel){
+    if (numberOfSlides > 1){
+      const $prevButton = $(document.createElement('button')).attr({class: 'left photocarousel-button noselect', name: 'previous'}).appendTo($photocarousel);
+      const $prevButtonSymbol = $(document.createElement('span')).attr({class: 'glyphicon glyphicon-chevron-left', 'aria-hidden': true}).appendTo($prevButton);
+      const $prevButtonName = $(document.createElement('span')).text('Previous').attr({class: 'sr-only'}).appendTo($prevButton);
+      const $nextButton = $(document.createElement('button')).attr({class: 'right photocarousel-button noselect', name: 'next'}).appendTo($photocarousel);
+      const $nextButtonSymbol = $(document.createElement('span')).attr({class: 'glyphicon glyphicon-chevron-right', 'aria-hidden': true}).appendTo($nextButton);
+      const $nextButtonName = $(document.createElement('span')).text('Next').attr({class: 'sr-only'}).appendTo($nextButton);
+    }
+    return;
   }
 
   function createPhotocarouselDiv(photoURL, photoTitle, $photocarousel) {
@@ -144,6 +159,7 @@ $(() => {
   }
 
   function createLoaderDiv($photocarousel) {
+
     $(document.createElement('div'))
       .attr('class', 'wait')
       .appendTo($photocarousel);
@@ -151,20 +167,34 @@ $(() => {
       .attr('class', 'loader')
       .attr('src', 'images/ajax-loader.gif')
       .appendTo($photocarousel.children('.wait'));
+
+    return;
   }
 
   function createSearchFunction(searchFunction, $photocarousel) {
-    // $(document.createElement('div')).attr('class', 'fetch-image').insertAfter($photocarousel);
     if (searchFunction) {
-    const $fetchImageDiv = $photocarousel.siblings('.fetch-image');
-    const $fetchImageForm = $(document.createElement('form')).attr({class: 'fetch-image-form', action: ""});
-    const $spanTitle = $(document.createElement('span')).text('FETCH NEW IMAGES');
-    const $inputGroup = $(document.createElement('div')).attr({class: 'input-group'});
-    const $fetchKeywords = $(document.createElement('input')).attr({type: 'text', class: 'form-control fetch-keywords', name: 'search-term', placeholder: 'keywords'});
-    const $inputGroupBtn = $(document.createElement('div')).attr({class: 'input-group-btn'});
-    const $submitBtn = $(document.createElement('button')).attr({class: 'btn btn-default', type: 'submit'});
-    const $submitBtnLogo = $(document.createElement('span')).attr({class: 'glyphicon glyphicon-search'});
-    $fetchImageDiv.append($fetchImageForm.append($spanTitle, $inputGroup.append($fetchKeywords, $inputGroupBtn.append($submitBtn.append($submitBtnLogo)))));
+    const $fetchImageDiv = $(document.createElement('div')).attr('class', 'fetch-image col-md-12 col-lg-12').insertAfter($photocarousel);
+    // const $fetchImageDiv = $photocarousel.siblings('.fetch-image');
+    const $fetchImageForm = $(document.createElement('form'))
+      .attr({class: 'fetch-image-form', action: ""});
+    const $labelTitle = $(document.createElement('label'))
+      .text('Search Terms');
+    const $inputGroup = $(document.createElement('div'))
+      .attr({class: 'input-group'});
+    const $fetchKeywords = $(document.createElement('input'))
+      .attr({type: 'text', class: 'form-control fetch-keywords', name: 'search-term', placeholder: 'Keywords to search for...'});
+    const $inputGroupBtn = $(document.createElement('div'))
+      .attr({class: 'input-group-btn'});
+    const $submitBtn = $(document.createElement('button'))
+      .attr({class: 'btn btn-default', type: 'submit'});
+    const $submitBtnLogo = $(document.createElement('span'))
+      .attr({class: 'glyphicon glyphicon-search'});
+    const $resetBtn = $(document.createElement('button'))
+      .text('Reset Images')
+      .attr({class: 'btn btn-default reset', type: 'button', disabled: true});
+    // const $resetBtnLogo = $(document.createElement('span'))
+    //   .attr({class: 'glyphicon glyphicon-search'});
+    $fetchImageDiv.append($fetchImageForm.append($labelTitle, $inputGroup.append($fetchKeywords, $inputGroupBtn.append($submitBtn.append($submitBtnLogo), $resetBtn))));
     }
   }
 
@@ -191,7 +221,8 @@ $(() => {
         complete: function() {
           $(this).toggleClass('activeSlide');
           $(this).css('left', "");
-          $photocarousel.data('active', true);
+          // $photocarousel.data('active', true);
+          $photocarousel.photocarouselData.active = true;
           return;
         }
       });
@@ -217,7 +248,8 @@ $(() => {
         complete: function() {
           $(this).toggleClass('activeSlide');
           $(this).css('left', "");
-          $photocarousel.data('active', true);
+          // $photocarousel.data('active', true);
+          $photocarousel.photocarouselData.active = true;
           return;
         }
       });
@@ -232,6 +264,14 @@ $(() => {
     event.preventDefault();
     const $photocarousel = event.data.photocarousel;
     const keywords = $(this).find(".fetch-keywords").val().trim().split(/\s+/).join(',');
+    try {
+      if (!keywords){
+        throw('Empty keywords not allowed')
+      }
+    } catch (e) {
+      alert(`Error: ${e}`);
+      return;
+    }
     const apiKey = "fd5f20a53c009a33506904c2ab164800";
     const flickrurl = "https://api.flickr.com/services/rest/";
     var xhr = $.ajax({
@@ -254,12 +294,13 @@ $(() => {
         console.log('not ok');
         return;
       } else {
-        const $removedDivs = $photocarousel.children('.photocarousel-div').detach();
+        // const $removedDivs = $photocarousel.children('.photocarousel-div').detach();
         const data = rsp.photos.photo;
-        $photocarousel.data('current-index', 0);
-        render(0, $photocarousel, 'fetch', {
+        // $photocarousel.data('current-index', 0);
+        $photocarousel.photocarouselData.currentIndex = 0;
+        render({
           data: data
-        });
+        }, 0, $photocarousel, 'fetch');
       }
       return;
     }).fail(function(jqXHR, textStatus, errorThrown) {
@@ -283,15 +324,25 @@ $(() => {
   function bindHandlers($photocarousel){
     const $fetchImageForm = $photocarousel.siblings('.fetch-image').find('.fetch-image-form');
     const $photocarouselButton = $photocarousel.children('.photocarousel-button');
+    const $resetButton = $photocarousel.siblings().find('.reset');
     $photocarouselButton.click({
       photocarousel: $photocarousel
     }, photocarouselButtonHandler);
     $fetchImageForm.submit({
       photocarousel: $photocarousel
     }, fetchImageHandler);
-
+    $resetButton.click({
+      photocarousel: $photocarousel
+    }, resetFetchHandler);
+    return;
   }
 
+  function resetFetchHandler(event){
+    console.log('reset clicked');
+    const $photocarousel = event.data.photocarousel;
+    const opts = $photocarousel.settings;
+    render(opts, opts.startingIndex, $photocarousel, 'init')
+  }
   /*
    * Initialises which slide to be displayed first
    *
@@ -300,8 +351,9 @@ $(() => {
     const defaults = {
       startingIndex: 0,
       numberOfSlides: 5,
-      searchFunction: true
+      searchFunction: false
     };
+
     const settings = $.extend(defaults, options);
     const startingIndex = settings.startingIndex;
     const numberOfSlides = settings.numberOfSlides;
@@ -310,26 +362,35 @@ $(() => {
     const $photocarouselContainer = $(photocarouselContainer);
     const $photocarousel = $photocarouselContainer.children('.photocarousel');
 
-    $photocarousel.data({
-      'current-index': startingIndex,
+    try {
+      if (options.startingIndex >= options.numberOfSlides){
+        throw('Index out of range of number of slides to be displayed');
+      }
+      if (photocarouselContainer === undefined || photocarouselContainer === "" || !$photocarouselContainer.length) {
+        throw('Please provide a valid selector for photocarouselContainer');
+      }
+    } catch (e) {
+      console.error(e);
+    }
+    $photocarousel.settings = settings;
+    $photocarousel.photocarouselData = {
+      currentIndex: startingIndex,
       active: true
-    });
-    render(startingIndex, $photocarousel, 'init', settings);
+    };
+    render(settings, startingIndex, $photocarousel, 'init');
     bindLoader('.wait');
     bindHandlers($photocarousel);
     return;
   }
 
-
   const options0 = {
     startingIndex: 0,
-    numberOfSlides: 1,
+    numberOfSlides: 5,
     searchFunction: true
   };
   const options1 = {
     startingIndex: 1,
-    numberOfSlides: 5,
-    searchFunction: false
+    numberOfSlides: 2,
   };
   init(options0, '#photocarousel0');
   init(options1, '#photocarousel1');
